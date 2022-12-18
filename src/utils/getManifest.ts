9923f66ff0cae3ac735cb845db4c77e5b09875d5
Icemourne <Cboweris@gmail.com>
2022-12-18 19:43:39 +0200

    update

--- src/utils/getManifest.ts
+++ src/utils/getManifest.ts
@@ -1,6 +1,7 @@
 import { InventoryItems } from '../interfaces/inventoryItem.interface'
 import { PlugSets } from '../interfaces/plugSet.interface'
 import { SocketTypes } from '../interfaces/socketType.interface'
+import { persistentFetch } from '@icemourne/tool-box'
 
 export interface Manifest {
    inventoryItems: InventoryItems
@@ -8,24 +9,17 @@ export interface Manifest {
    socketType: SocketTypes
 }
 
-const justFetch = async (link: string, retries: number = 0): Promise<any> => {
-   const resp = await fetch(link)
-   if (resp.status === 200) return await resp.json()
-   if (retries > 5) return
-   return await justFetch(link, retries + 1)
-}
-
 export const getManifest = async () => {
-   const manifest = await justFetch('https://www.bungie.net/Platform/Destiny2/Manifest/')
+   const manifest = await persistentFetch('https://www.bungie.net/Platform/Destiny2/Manifest/', 5)
    if (manifest === undefined) throw new Error('failed getting manifest')
 
    const inventoryItemUrl = `https://www.bungie.net${manifest.Response.jsonWorldComponentContentPaths.en.DestinyInventoryItemDefinition}`,
       plugSetUrl = `https://www.bungie.net${manifest.Response.jsonWorldComponentContentPaths.en.DestinyPlugSetDefinition}`,
       socketTypeUrl = `https://www.bungie.net${manifest.Response.jsonWorldComponentContentPaths.en.DestinySocketTypeDefinition}`
 
-   const inventoryItems = await justFetch(inventoryItemUrl),
-      plugSet = await justFetch(plugSetUrl),
-      socketType = await justFetch(socketTypeUrl)
+   const inventoryItems = await persistentFetch(inventoryItemUrl, 5),
+      plugSet = await persistentFetch(plugSetUrl, 5),
+      socketType = await persistentFetch(socketTypeUrl, 5)
 
    if (inventoryItems === undefined || plugSet === undefined || socketType === undefined)
       throw new Error('Failed getting data')
