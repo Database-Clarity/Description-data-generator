7ad874ca152f8f7efa3dc113f4ed35f874e6c990
Icemourne <Cboweris@gmail.com>
2023-03-26 19:50:14 +0300

    fidex some mods

--- src/filters/armorMod.ts
+++ src/filters/armorMod.ts
@@ -6,58 +6,62 @@ import { SocketCategoryEnums } from '../utils/enums.js'
 import { getAllFromSocket } from '../utils/getAllFromSocket.js'
 
 export const armorMods = (
-   inventoryItems: InventoryItems,
-   plugSets: PlugSets,
-   inventoryItemLegendaryArmor: InventoryItem[]
+  inventoryItems: InventoryItems,
+  plugSets: PlugSets,
+  inventoryItemLegendaryArmor: InventoryItem[]
 ) => {
-   const data: { [key: string]: PerkData } = {}
+  const data: { [key: string]: PerkData } = {}
 
-   const addData = (armor: InventoryItem, perk: InventoryItem, type: PerkTypes) => {
-      const armorType = armor.itemTypeDisplayName
-      if (armorType === undefined) return
+  const addData = (armor: InventoryItem, perk: InventoryItem, type: PerkTypes) => {
+    const armorType = armor.itemTypeDisplayName
+    if (armorType === undefined) return
 
-      if (data[perk.hash] !== undefined) {
-         data[perk.hash].appearsOn.add(armorType)
-         return
-      }
+    if (data[perk.hash] !== undefined) {
+      data[perk.hash].appearsOn.add(armorType)
+      return
+    }
 
-      data[perk.hash] = {
-         appearsOn: new Set([armorType]),
-         name: perk.displayProperties.name,
-         hash: Number(perk.hash),
-         type
-      }
-   }
+    data[perk.hash] = {
+      appearsOn: new Set([armorType]),
+      name: perk.displayProperties.name,
+      hash: Number(perk.hash),
+      type
+    }
+  }
 
-   inventoryItemLegendaryArmor.forEach((armor) => {
-      const armorSockets = armor.sockets
-      if (armorSockets === undefined) return
+  inventoryItemLegendaryArmor.forEach((armor) => {
+    const armorSockets = armor.sockets
+    if (armorSockets === undefined) return
 
-      const modSocketCategory = armorSockets.socketCategories.find(
-         (socketCategory) => socketCategory.socketCategoryHash === SocketCategoryEnums.armorMods
-      )
+    const modSocketCategory = armorSockets.socketCategories.find(
+      (socketCategory) => socketCategory.socketCategoryHash === SocketCategoryEnums.armorMods
+    )
 
-      modSocketCategory?.socketIndexes.forEach((socketIndex) => {
-         const modsArr = getAllFromSocket(inventoryItems, plugSets, armorSockets.socketEntries[socketIndex])
+    modSocketCategory?.socketIndexes.forEach((socketIndex) => {
+      const modsArr = getAllFromSocket(inventoryItems, plugSets, armorSockets.socketEntries[socketIndex])
 
-         modsArr.forEach((modHash) => {
-            const mod = inventoryItems[modHash]
+      modsArr.forEach((modHash) => {
+        const mod = inventoryItems[modHash]
 
-            if (mod?.itemTypeDisplayName?.match(/(General|Helmet|Arms|Chest|Leg) Armor Mod|Class Item Mod/)) {
-               addData(armor, mod, 'Armor Mod General')
-               return
-            }
-            if (mod?.itemTypeDisplayName?.match(/(Elemental Well|Charged with Light|Warmind Cell) Mod/)) {
-               addData(armor, mod, 'Armor Mod Combat')
-               return
-            }
-            if (mod?.itemTypeDisplayName?.match(/ Raid Mod$|Nightmare Mod|King's Fall Mod|Vault of Glass Armor Mod/)) {
-               addData(armor, mod, 'Armor Mod Activity')
-               return
-            }
-         })
+        if (mod?.itemTypeDisplayName?.match(/(General|Helmet|Arms|Chest|Leg|Class Item) Armor Mod/)) {
+          addData(armor, mod, 'Armor Mod General')
+          return
+        }
+        if (mod?.itemTypeDisplayName?.match(/(Elemental Well|Charged with Light|Warmind Cell) Mod/)) {
+          addData(armor, mod, 'Armor Mod Combat')
+          return
+        }
+        if (
+          mod?.itemTypeDisplayName?.match(
+            /( Raid|Nightmare|King's Fall|Vault of Glass|Root of Nightmares)( Armor)? Mod$/
+          )
+        ) {
+          addData(armor, mod, 'Armor Mod Activity')
+          return
+        }
       })
-   })
+    })
+  })
 
-   return data
+  return data
 }
